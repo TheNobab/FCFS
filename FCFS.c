5dac05b756656261a8b17083244c8cee60b6f8ea
partha roy <79916168+TheNobab@users.noreply.github.com>
2024-02-13 11:23:08 +0600

    Add files via upload

--- FCFS.c
+++ FCFS.c
@@ -1,65 +1,76 @@
-
-//First Come First Serve (FCFS) CPU time scheduling
 #include<stdio.h>
 
 int main(){
-    int i=0,t=0,n=0,p[10],at[10],bt[10],ct[10],tat[10],wt[10],rt[10],avg_tat=0,ttat=0,tp=0,temp=0;
+    int i=0,t=0,p[10],at[10],bt[10],ct[10],tat[10],wt[10],rt[10],avg_tat=0,ttat=0,tp=0,temp=0;
     printf("how many process: ");
     scanf("%d",&t);
-//assiging all the value of process no,arival time,burst time.
+    //assigning all the values of process no, arrival time, burst time
     for(i=1;i<=t;i++){
         printf("Process no: ");
         scanf("%d",&p[i]);
-        printf("Arival time: ");
+        printf("Arrival time: ");
         scanf("%d",&at[i]);
         printf("Burst time: ");
         scanf("%d",&bt[i]);
         printf("\n");
     }
 
-// Sorting the numbers in ascending order
-    for (i = 0; i < 5; i++) {
-        for (int j = i + 1; j < 5; j++) {
+    // Sorting the processes based on arrival time in ascending order
+    for (i = 1; i <= t; i++) {
+        for (int j = i + 1; j <= t; j++) {
             if (at[i] > at[j]) {
                 temp = at[i];
                 at[i] = at[j];
                 at[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
     }
-//calculating completion time
-     for (i=1;i<=t;i++){
-            if(at[i]<=at[t]){
-                ct[i]=at[i]+bt[i];
-            }
+
+    //calculating completion time
+    for (i = 1; i <= t; i++){
+        if (at[i] <= ct[i-1]){
+            ct[i] = ct[i-1] + bt[i];
+        } else {
+            ct[i] = at[i] + bt[i];
+        }
     }
 
-//calculating turn around time { TAT = CT - AT}
-     for (i=1;i<=t;i++){
-            tat[i]=ct[i]-at[i];
+    //calculating turn around time { TAT = CT - AT}
+    for (i = 1; i <= t; i++){
+        tat[i] = ct[i] - at[i];
+    }
 
+    //calculating waiting time {WT = TAT-BT}
+    for (i = 1; i <= t; i++){
+        wt[i] = tat[i] - bt[i];
     }
-//calculating waiting time {WT = TAT-BT}
-     for (i=1;i<=t;i++){
-            wt[i]=tat[i]-bt[i];
-    }
-//calculating response time {response time =  the first time process set cpu - AT}
-     for (i=1;i<=t;i++){
-         rt[i]= ct[i-1]-at[i];
-    }
-//calculating Average Total around time {AVG TAT = Total sum TAT / Total process}
-     for (i=1;i<=t;i++){
-    ttat += tat[i]; // ttat means total time around time
-    }
-//Display the Average total around time
-    avg_tat = ttat/t;
-    printf("avarage total around time: %d\n\n",avg_tat);
 
-
-//Display the final CPU process schedule
-    printf("process no     Arival Time     Burst Time     Completion Time    Turn around time     Waiting Time     Response Time \n");
-     for (i=1;i<=t;i++){
-    printf("   %d               %d             %d              %d                %d                   %d                 %d      \n" ,i,at[i],bt[i],ct[i],tat[i],wt[i],rt[i]);
+    //calculating response time {response time = the first time process sets CPU - AT}
+    rt[1] = 0; // Initializing the first process's response time to 0
+    for (i = 2; i <= t; i++){
+        rt[i] = ct[i-1] - at[i];
     }
-return 0;
+
+    //calculating Average Total turn around time {AVG TAT = Total sum TAT / Total processes}
+    for (i = 1; i <= t; i++){
+        ttat += tat[i]; // ttat means total turn around time
+    }
+    //Display the Average total turn around time
+    avg_tat = ttat / t;
+    printf("average total turn around time: %d\n\n", avg_tat);
+
+    //Display the final CPU process schedule
+    printf("Process no\tArrival Time\tBurst Time\tCompletion Time\tTurn around time\tWaiting Time\tResponse Time\n");
+    for (i = 1; i <= t; i++){
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t%d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
+    }
+    return 0;
 }
